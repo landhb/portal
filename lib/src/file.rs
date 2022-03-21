c01309bf1b3cd146c326635b61c698e28685a4ff
landhb <landhb@users.noreply.github.com>
2022-03-20 22:24:47 -0400

    Add test for failure path on decrypt(), assist with codecov

--- lib/src/file.rs
+++ lib/src/file.rs
@@ -59,7 +59,7 @@ impl PortalFile {
         // Generate random nonce
         let mut rng = rand::thread_rng();
         let rbytes = rng.gen::<[u8; 12]>();
-        let nonce = Nonce::from_slice(&rbytes); // 128-bits; unique per chunk
+        let nonce = Nonce::from_slice(&rbytes); // 128-bits; Used once for entire file
         self.state.nonce.extend(nonce);
 
         let tag = match self
@@ -167,10 +167,7 @@ impl PortalFile {
         &'a self,
         chunk_size: usize,
     ) -> impl std::iter::Iterator<Item = &'a [u8]> {
-        PortalChunks::init(
-            &self.mmap[..], // TODO: verify that this is zero-copy
-            chunk_size,
-        )
+        PortalChunks::init(&self.mmap[..], chunk_size)
     }
 
     /**
@@ -185,6 +182,7 @@ impl PortalFile {
 
 #[cfg(test)]
 pub mod tests {
+    use crate::errors::PortalError;
     use crate::{Direction, Portal};
     use std::io::{Read, Write};
 
@@ -210,6 +208,47 @@ pub mod tests {
         fn flush(&mut self) -> Result<(), std::io::Error> {
             Ok(())
         }
+    }
+
+    #[test]
+    fn test_failed_decryption() {
+        // receiver
+        let dir = Direction::Receiver;
+        let pass = "test".to_string();
+        let (mut receiver, receiver_msg) = Portal::init(dir, "id".to_string(), pass, None);
+
+        // sender
+        let dir = Direction::Sender;
+        let pass = "test".to_string();
+        let (mut sender, sender_msg) = Portal::init(dir, "id".to_string(), pass, None);
+
+        // we need a key to be able to encrypt
+        receiver.derive_key(sender_msg.as_slice()).unwrap();
+        sender.derive_key(receiver_msg.as_slice()).unwrap();
+
+        // encrypt the file
+        let mut file = sender.load_file("/etc/passwd").unwrap();
+        file.encrypt().unwrap();
+
+        // Test incorrect tag length path
+        let old_tag = file.state.tag.clone();
+        file.state.tag.truncate(0);
+        let result = file.decrypt();
+        assert!(result.is_err());
+        let _ = result.map_err(|e| match e.downcast_ref::<PortalError>() {
+            Some(PortalError::DecryptError) => anyhow::Ok(()),
+            _ => panic!("Unexpected error"),
+        });
+
+        // Test failed decryption path
+        file.state.tag = old_tag;
+        file.state.tag[0] += 1; // alter tag
+        let result = file.decrypt();
+        assert!(result.is_err());
+        let _ = result.map_err(|e| match e.downcast_ref::<PortalError>() {
+            Some(PortalError::DecryptError) => anyhow::Ok(()),
+            _ => panic!("Unexpected error"),
+        });
     }
 
     #[test]
