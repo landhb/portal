8a908cb0dd51a0983dbee9479a305f4155162823
landhb <landhb@users.noreply.github.com>
2022-03-27 10:55:28 -0400

    Add receive benchmakrs, remove 500M tests for now.

--- lib/benches/sending.rs
+++ lib/benches/sending.rs
@@ -3,7 +3,7 @@ use std::fs::File;
 use tempdir::TempDir;
 use std::io::Write;
 use portal::{Portal, Direction};
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
 
 /// Common to all sender tests
 fn setup() -> Portal {
@@ -50,65 +50,63 @@ fn bench_file_sender(c: &mut Criterion) {
     c.bench_function("encrypt & send 100k", |b| b.iter(|| {
         let mut file = sender.load_file(&path).unwrap();
         file.encrypt().unwrap();
-        let chunk_size = 65536;
         let mut total_size = 0;
-        for v in file.get_chunks(chunk_size) {
-            assert!(v.len() <= chunk_size);
+        for v in file.get_chunks(portal::CHUNK_SIZE) {
+            assert!(v.len() <= portal::CHUNK_SIZE);
             total_size += v.len();
-            black_box(v);
         }
         assert!(total_size >= 100_000);
     }));
 
 
-    // Benchmark loading the file and iterating over the chunks
-    // this allows to compare chunking vs single pass encryption
+    // 1M
     let path = create_file(&tmp_dir, 1_000_000);
     c.bench_function("encrypt & send 1M", |b| b.iter(|| {
         let mut file = sender.load_file(&path).unwrap();
         file.encrypt().unwrap();
-        let chunk_size = 65536;
         let mut total_size = 0;
-        for v in file.get_chunks(chunk_size) {
-            assert!(v.len() <= chunk_size);
+        for v in file.get_chunks(portal::CHUNK_SIZE) {
+            assert!(v.len() <= portal::CHUNK_SIZE);
             total_size += v.len();
-            black_box(v);
         }
         assert!(total_size >= 1_000_000);
     }));
 
 
-    // Benchmark loading the file and iterating over the chunks
-    // this allows to compare chunking vs single pass encryption
+    // 100M
     let path = create_file(&tmp_dir, 100_000_000);
     c.bench_function("encrypt & send 100M", |b| b.iter(|| {
         let mut file = sender.load_file(&path).unwrap();
         file.encrypt().unwrap();
-        let chunk_size = 65536;
         let mut total_size = 0;
-        for v in file.get_chunks(chunk_size) {
-            assert!(v.len() <= chunk_size);
+        for v in file.get_chunks(portal::CHUNK_SIZE) {
+            assert!(v.len() <= portal::CHUNK_SIZE);
             total_size += v.len();
-            black_box(v);
         }
         assert!(total_size >= 100_000_000);
     }));
 
-    // Benchmark loading the file and iterating over the chunks
-    // this allows to compare chunking vs single pass encryption
-    let path = create_file(&tmp_dir, 1_000_000_000);
-    c.bench_function("encrypt & send 1G", |b| b.iter(|| {
+
+    // Configure Criterion.rs with larger measurement times
+    /* for larger files.
+    let mut group = c.benchmark_group("larger-files");
+    group.measurement_time(core::time::Duration::new(60,0));
+
+    //500M
+    let path = create_file(&tmp_dir, 500_000_000);
+    group.bench_function("encrypt & send 500M", |b| b.iter(|| {
         let mut file = sender.load_file(&path).unwrap();
         file.encrypt().unwrap();
-        let chunk_size = 65536;
         let mut total_size = 0;
-        for v in file.get_chunks(chunk_size) {
-            assert!(v.len() <= chunk_size);
+        for v in file.get_chunks(portal::CHUNK_SIZE) {
+            assert!(v.len() <= portal::CHUNK_SIZE);
             total_size += v.len();
-            black_box(v);
         }
-        assert!(total_size >= 1_000_000_000);
+        assert!(total_size >= 500_000_000);
     }));
+
+
+    group.finish(); */
 }
 
 
